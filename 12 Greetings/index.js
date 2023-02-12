f8079cd63e3ed2596cdd22834e5957539ca6075c
Ashraf Hussain <125198812+AshrafPanaverse@users.noreply.github.com>
2023-02-12 22:30:50 +0500

    upto 12

---
+++ 12 Greetings/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let frinds_name = ["Asad", "Babar", "Shahzaib", "Bilal", "Anber", "Asher"];
+console.log(`${frinds_name[0]} is my friend.`);
+console.log(`${frinds_name[1]} is my friend.`);
+console.log(`${frinds_name[2]} is my friend.`);
+console.log(`${frinds_name[3]} is my friend.`);
+console.log(`${frinds_name[4]} is my friend.`);
+console.log(`${frinds_name[5]} is my friend.`);
---
+++ 12 Greetings/index.ts
@@ -0,0 +1,7 @@
+let frinds_name=["Asad","Babar","Shahzaib","Bilal","Anber","Asher"]
+console.log(`${frinds_name[0]} is my friend.`);
+console.log(`${frinds_name[1]} is my friend.`);
+console.log(`${frinds_name[2]} is my friend.`);
+console.log(`${frinds_name[3]} is my friend.`);
+console.log(`${frinds_name[4]} is my friend.`);
+console.log(`${frinds_name[5]} is my friend.`);
