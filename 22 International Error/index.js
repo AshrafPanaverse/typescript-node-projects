bba8c2fb6d1f5cd8d5d3036cdb8f116d55c8e0d8
Ashraf Hussain <125198812+AshrafPanaverse@users.noreply.github.com>
2023-02-13 22:33:34 +0500

    upto exercise 22

---
+++ 22 International Error/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var my_list = ["water", "sugar", 'salt', 'vegitable', 'Mango'];
+console.log("Intentional Error calling for console element at 6 which is out of range");
+if (my_list.length > 6) {
+    console.log(my_list[6]);
+}
+else {
+    console.log('Index error: the index is out of bounds for the array.');
+}
